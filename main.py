2296ab7951a6bec25a9b2046a05565c49d1cfc82
Mkhuzo <zulumkhuzo@gmail.com>
2023-08-17 21:07:32 +0200

    add scaler

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 import joblib
 from pydantic import BaseModel, conlist
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 import numpy as np
 
 app = FastAPI(title="Trade Analyser ML API", description="API for tade model", version="1.0")
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Declare Filenames
 filename_model = "trade_analyser_model.joblib"
@@ -31,8 +45,8 @@ async def predict_emotion(features: Features):
             "error": "only 2 dimensional arrays allowed"
         }
 
-    squad1_preds = loaded_model.predict(squad1).tolist()
-    squad2_preds = loaded_model.predict(squad2).tolist()
+    squad1_preds = loaded_model.predict(loaded_scaler.transform(squad1)).tolist()
+    squad2_preds = loaded_model.predict(loaded_scaler.transform(squad2)).tolist()
 
     return {
         "first_squad": squad1_preds,
